a4903388fffc8b96c973b554f2b6b5aef87d02f3
alexandr <alex-trent@yandex.ru>
2023-03-11 20:52:57 +0700

    решения DZPY

---
+++ task_6.py
@@ -0,0 +1,11 @@
+
+m = input("Введите 6-ти значное число: ")
+a = int(m[0]) + int(m[1]) + int(m[2])
+b = int(m[3]) + int(m[4]) + int(m[5])
+if a == b:
+    print('Yes')
+else:
+    print('No')
+
+    
+    
